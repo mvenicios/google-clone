2e9848df77dfb3123e735e8c6bc2b1e47c543e5a
mauro <mvenicios@bol.com.br>
2023-04-25 20:03:48 -0300

    add body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,21 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      {/*header*/}
       <HomeHeader />
+      {/*body*/}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width="300"
+          height="100"
+          src="https://img1.picmix.com/output/stamp/normal/3/1/6/7/1737613_80338.png"
+        />
+        <HomeSearch />
+      </div>
     </>
   );
 }
